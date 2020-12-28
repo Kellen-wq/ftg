797c378ba19b4eef88b257c59c7bb9344ef3bd66
Kellen-wq <76692731+Kellen-wq@users.noreply.github.com>
2020-12-28 16:44:56 +0300

    Update ok.py

--- ok.py
+++ ok.py
@@ -3,6 +3,7 @@ import asyncio
 import os
 import sys
 i = 1000
+s = i - 7
 
 @borg.on(events.NewMessage(pattern=r"\.ok", outgoing=True))
 async def _(event):
@@ -12,20 +13,20 @@ async def _(event):
 
     await event.edit("1000")
     await asyncio.sleep(1)
-    await event.edit(i-7)
+    await event.edit(s)
     await asyncio.sleep(1)
-    await event.edit(i-7)
+    await event.edit(s)
     await asyncio.sleep(1)
-    await event.edit(i-7)
+    await event.edit(s)
     await asyncio.sleep(1)
-    await event.edit(i-7)
+    await event.edit(s)
     await asyncio.sleep(1)
-    await event.edit(i-7)
+    await event.edit(s)
     await asyncio.sleep(1)
-    await event.edit(i-7)
+    await event.edit(s)
     await asyncio.sleep(1)
-    await event.edit(i-7)
+    await event.edit(s)
     await asyncio.sleep(1)
-    await event.edit(i-7)
+    await event.edit(s)
     await asyncio.sleep(1)
     await event.edit("БОМБИТ С ТЕБЯ!")
